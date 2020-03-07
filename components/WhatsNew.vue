69b0e2f547fd6c4cd97f2ad3fef6fe122b8add53
kawoka <br2jn51@yahoo.co.jp>
2020-03-07 20:13:59 +0900

    test fail

--- components/WhatsNew.vue
+++ components/WhatsNew.vue
@@ -11,11 +11,7 @@
         <time class="WhatsNew-item-time px-2">{{ item.date }}</time>
         <span class="WhatsNew-item-link">
           {{ item.text }}
-          <v-icon
-            v-if="!isInternalLink(item.url)"
-            class="WhatsNew-item-ExternalLinkIcon"
-            size="12"
-          >
+          <v-icon v-if="!isInternalLink(item.url)" class="WhatsNew-item-ExternalLinkIcon" size="12" >
             mdi-open-in-new
           </v-icon>
         </span>
